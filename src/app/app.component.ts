5cd350d1f18d512875a9c49822e4866073b07066
AlMuz <almuz@inbox.lv>
2021-03-11 17:22:11 +0200

    async pipe and observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 export interface Post {
   title: string;
@@ -33,6 +34,18 @@ export class AppComponent {
     { title: 'Cheese', text: 'Yummy!' },
     { title: 'Potato', text: 'Food for good guys!' },
   ];
+
+  p: Promise<string> = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('Promise resolved');
+    }, 4000);
+  });
+
+  obsDate: Observable<Date> = new Observable((obs) => {
+    setInterval(() => {
+      obs.next(new Date());
+    }, 1000);
+  });
 
   addPosts() {
     this.posts.push({
